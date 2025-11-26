90f6fe262b17d2af2464209954555ae69065ca97
dingaiqi <1621029739@qq.com>
2025-11-26 17:38:18 +0800

    轴-增加ui

--- src/components/AxisPlane/AxisPlane.vue
+++ src/components/AxisPlane/AxisPlane.vue
@@ -1,65 +1,130 @@
-<!--
- * @Author: dys
- * @Date: 2025-11-24 16:59:03
- * @LastEditors: dys
- * @LastEditTime: 2025-11-26 14:58:11
- * @Descripttion: 
--->
 <template>
   <div>
     <div id="cesiumContainer"></div>
+    
+    <!-- Element UI 控制面板 -->
+    <el-card class="control-panel" v-if="showControls">
+      <div slot="header" class="clearfix">
+        <span>3D 对象控制</span>
+        <el-button 
+          style="float: right; padding: 3px 0" 
+          type="text"
+          @click="toggleControls"
+        >
+          隐藏
+        </el-button>
+      </div>
+      
+      <!-- 点实体控制 -->
+      <el-collapse v-model="activeNames">
+        <el-collapse-item title="点实体控制" name="1">
+          <div class="control-item">
+            <el-checkbox v-model="pointControls.translateEnabled" @change="updatePointControls">
+              启用平移
+            </el-checkbox>
+          </div>
+        </el-collapse-item>
+        
+        <!-- 模型控制 -->
+        <el-collapse-item title="模型控制" name="2">
+          <div class="control-item">
+            <el-checkbox v-model="modelControls.rotateEnabled" @change="updateModelControls">
+              启用旋转
+            </el-checkbox>
+          </div>
+          <div class="control-item">
+            <el-checkbox v-model="modelControls.translateEnabled" @change="updateModelControls">
+              启用平移
+            </el-checkbox>
+          </div>
+          <div class="control-item">
+            <el-checkbox v-model="modelControls.scaleEnabled" @change="updateModelControls">
+              启用缩放
+            </el-checkbox>
+          </div>
+        </el-collapse-item>
+        
+        <!-- 3D Tiles 控制 -->
+        <el-collapse-item title="3D Tiles 控制" name="3">
+          <div class="control-item">
+            <el-checkbox v-model="tilesControls.rotateEnabled" @change="updateTilesControls">
+              启用旋转
+            </el-checkbox>
+          </div>
+          <div class="control-item">
+            <el-checkbox v-model="tilesControls.translateEnabled" @change="updateTilesControls">
+              启用平移
+            </el-checkbox>
+          </div>
+        </el-collapse-item>
+      </el-collapse>
+    </el-card>
+    
+    <!-- 显示/隐藏控制面板的按钮 -->
+    <el-button 
+      v-if="!showControls"
+      class="toggle-button" 
+      type="primary" 
+      icon="el-icon-s-operation"
+      circle
+      @click="toggleControls"
+    ></el-button>
   </div>
 </template>
 
 <script>
-import * as dat from "dat.gui";
 import "cesium/Widgets/widgets.css";
 import * as Cesium from "cesium/Cesium";
 import EditCesium from "./js/EditCesium";
-export default {
-  components: {},
 
+export default {
   data() {
     return {
       viewer: null,
       point: null,
       model: null,
       tileset: null,
+      pointEditCesium: null,
+      modelEditCesium: null,
+      tilesEditCesium: null,
+      showControls: true,
+      activeNames: ['1', '2', '3'],
+      pointControls: {
+        translateEnabled: true
+      },
+      modelControls: {
+        rotateEnabled: true,
+        translateEnabled: true,
+        scaleEnabled: true
+      },
+      tilesControls: {
+        rotateEnabled: true,
+        translateEnabled: true
+      }
     };
   },
 
-  computed: {},
-
   mounted() {
-    Cesium.Ion.defaultAccessToken =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0ZjI1YWYwNS00ODJiLTQzOTYtYTA3My0zMzI3ODFiZTdkYzAiLCJpZCI6MjAzOTgsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NzczNDAzMjN9.s1TyB5DYncqM_xAF-ekua_P4fcZmmyeVR4hA9ec9G4Q";
+    Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0ZjI1YWYwNS00ODJiLTQzOTYtYTA3My0zMzI3ODFiZTdkYzAiLCJpZCI6MjAzOTgsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NzczNDAzMjN9.s1TyB5DYncqM_xAF-ekua_P4fcZmmyeVR4hA9ec9G4Q";
+    
     this.viewer = new Cesium.Viewer("cesiumContainer", {
-      // scene3DOnly:true,
-      homeButton: false, // 是否显示home控件
+      homeButton: false,
       vrButton: false,
       fullscreenButton: true,
-      animation: true, // 是否显示动画控件
-      geocoder: false, // 是否显示地名查找控件
-      timeline: true, // 是否显示时间线控件
-      sceneModePicker: false, // 是否显示投影方式控件
-      navigationHelpButton: false, // 是否显示帮助信息控件
-      infoBox: false, // 是否显示点击要素之后显示的信息
-      baseLayerPicker: false, // 是否显示图层选择控件
-      selectionIndicator: true, // Disable selection indicator
+      animation: true,
+      geocoder: false,
+      timeline: true,
+      sceneModePicker: false,
+      navigationHelpButton: false,
+      infoBox: false,
+      baseLayerPicker: false,
+      selectionIndicator: true,
       shouldAnimate: true,
-      // requestRenderMode: true
     });
+    
     this.viewer._cesiumWidget._creditContainer.style.display = "none";
-    this.viewer.animation.container.style.visibility = "hidden"; // 不显示动画控件
-    this.viewer.timeline.container.style.visibility = "hidden"; // 不显示时间控件
-
-    // this.viewer.camera.flyTo({
-    //   destination: Cesium.Cartesian3.fromDegrees(116.39801, 39.89869, 500),
-    //   orientation: {
-    //     heading: Cesium.Math.toRadians(0),
-    //     pitch: Cesium.Math.toRadians(-90), // 俯仰角（低头看）
-    //   },
-    // });
+    this.viewer.animation.container.style.visibility = "hidden";
+    this.viewer.timeline.container.style.visibility = "hidden";
 
     this.addPointEntity();
     this.addModel();
@@ -75,28 +140,48 @@ export default {
           color: Cesium.Color.RED,
         },
       });
-      const pointEditCesium = new EditCesium(this.viewer, {
-        translateEnabled: true,
+      
+      this.pointEditCesium = new EditCesium(this.viewer, {
+        translateEnabled: this.pointControls.translateEnabled,
       });
-      pointEditCesium.addTo(this.point);
+      this.pointEditCesium.addTo(this.point);
     },
+    
+    updatePointControls() {
+      if (this.pointEditCesium) {
+        this.pointEditCesium.translateEnabled = this.pointControls.translateEnabled;
+      }
+    },
+
     async addModel() {
       const origin = Cesium.Cartesian3.fromDegrees(116.39643, 39.89863, 50);
       const modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(origin);
+      
       this.model = await Cesium.Model.fromGltfAsync({
         url: "/data/gltf/Cesium_Air.glb",
         modelMatrix: modelMatrix,
         scale: 10,
       });
+      
       this.viewer.scene.primitives.add(this.model);
 
-      const modelEditCesium = new EditCesium(this.viewer, {
-        rotateEnabled: true,
-        translateEnabled: true,
-        scaleEnabled: true,
+      this.modelEditCesium = new EditCesium(this.viewer, {
+        rotateEnabled: this.modelControls.rotateEnabled,
+        translateEnabled: this.modelControls.translateEnabled,
+        scaleEnabled: this.modelControls.scaleEnabled,
       });
-      modelEditCesium.addTo(this.model);
+      
+      this.modelEditCesium.addTo(this.model);
     },
+    
+    updateModelControls() {
+      if (this.modelEditCesium) {
+        this.modelEditCesium.rotateEnabled = this.modelControls.rotateEnabled;
+        this.modelEditCesium.translateEnabled = this.modelControls.translateEnabled;
+        this.modelEditCesium.scaleEnabled = this.modelControls.scaleEnabled;
+      }
+    },
+
     async add3dtiles() {
       this.tileset = await Cesium.Cesium3DTileset.fromUrl(
         `data/3dtiles/bim/tileset.json`,
@@ -109,23 +194,56 @@ export default {
           debugShowContentBoundingVolume: false,
         }
       );
+      
       this.viewer.scene.primitives.add(this.tileset);
       this.viewer.zoomTo(this.tileset);
 
-      const bimEditCesium = new EditCesium(this.viewer, {
-        rotateEnabled: true,
-        translateEnabled: true,
+      this.tilesEditCesium = new EditCesium(this.viewer, {
+        rotateEnabled: this.tilesControls.rotateEnabled,
+        translateEnabled: this.tilesControls.translateEnabled,
       });
-      bimEditCesium.addTo(this.tileset);
+      
+      this.tilesEditCesium.addTo(this.tileset);
     },
+    
+    updateTilesControls() {
+      if (this.tilesEditCesium) {
+        this.tilesEditCesium.rotateEnabled = this.tilesControls.rotateEnabled;
+        this.tilesEditCesium.translateEnabled = this.tilesControls.translateEnabled;
+      }
+    },
+    
+    toggleControls() {
+      this.showControls = !this.showControls;
+    }
   },
 };
 </script>
-<style>
-.control {
+
+<style scoped>
+#cesiumContainer {
+  width: 100%;
+  height: 100vh;
+  position: relative;
+}
+
+.control-panel {
   position: absolute;
-  z-index: 999;
-  left: 10px;
-  top: 10px;
+  top: 20px;
+  right: 20px;
+  width: 300px;
+  z-index: 1000;
+  background-color: rgba(255, 255, 255, 0.9);
+}
+
+.toggle-button {
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  z-index: 1000;
+}
+
+.control-item {
+  margin-bottom: 10px;
 }
 </style>
